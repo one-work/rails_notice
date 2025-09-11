b3d7db06a2978f76ad4edb6129f8e75cd8d91c5a
qinmingyuan <mingyuan0715@foxmail.com>
2025-09-11 20:52:20 +0800

    r

--- config/routes.rb
+++ config/routes.rb
@@ -1,65 +1,63 @@
 Rails.application.routes.draw do
-  scope RailsCom.default_routes_scope do
-    concern :notice do
-      resources :notifications, only: [:index, :show, :destroy] do
-        collection do
-          get :readed
-          post :read_all
-        end
+  concern :notice do
+    resources :notifications, only: [:index, :show, :destroy] do
+      collection do
+        get :readed
+        post :read_all
+      end
+      member do
+        get :url
+        patch :read
+        match :archive, via: [:put, :patch]
+      end
+    end
+    resource :notification_setting, only: [:show, :edit, :update]
+  end
+
+  namespace :notice, defaults: { business: 'notice' } do
+    namespace :admin, defaults: { namespace: 'admin' } do
+      root 'home#index'
+      resources :user_notifications do
         member do
-          get :url
-          patch :read
-          match :archive, via: [:put, :patch]
+          patch :push
+          patch :email
         end
       end
-      resource :notification_setting, only: [:show, :edit, :update]
+      resources :member_notifications do
+        member do
+          patch :push
+          patch :email
+        end
+      end
+      resources :notify_settings do
+        collection do
+          get :columns
+        end
+      end
+      resources :users
+      resources :members
+      resources :announcements do
+        member do
+          match :edit_user, via: [:get, :post]
+          patch 'user' => :update_user
+          get 'publish/options' => :options
+          match :publish, via: [:get, :post]
+        end
+        resources :announcement_user_tags
+        resources :announcement_organs
+      end
     end
 
-    namespace :notice, defaults: { business: 'notice' } do
-      namespace :admin, defaults: { namespace: 'admin' } do
-        root 'home#index'
-        resources :user_notifications do
-          member do
-            patch :push
-            patch :email
-          end
-        end
-        resources :member_notifications do
-          member do
-            patch :push
-            patch :email
-          end
-        end
-        resources :notify_settings do
-          collection do
-            get :columns
-          end
-        end
-        resources :users
-        resources :members
-        resources :announcements do
-          member do
-            match :edit_user, via: [:get, :post]
-            patch 'user' => :update_user
-            get 'publish/options' => :options
-            match :publish, via: [:get, :post]
-          end
-          resources :announcement_user_tags
-          resources :announcement_organs
-        end
-      end
+    namespace :me, defaults: { namespace: 'me' } do
+      concerns :notice
+    end
 
-      namespace :me, defaults: { namespace: 'me' } do
-        concerns :notice
-      end
+    namespace :my, defaults: { namespace: 'my' } do
+      concerns :notice
+    end
 
-      namespace :my, defaults: { namespace: 'my' } do
-        concerns :notice
-      end
-
-      namespace :board, defaults: { namespace: 'board' } do
-        concerns :notice
-      end
+    namespace :board, defaults: { namespace: 'board' } do
+      concerns :notice
     end
   end
 end
