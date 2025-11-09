b5aeba3f983ffc136285216df2d3b87940d1255c
qinmingyuan <mingyuan0715@foxmail.com>
2025-11-09 20:25:16 +0800

    add user tags

---
+++ app/controllers/notice/admin/announcement_user_tags_controller.rb
@@ -0,0 +1,31 @@
+module Notice
+  class Admin::AnnouncementUserTagsController < Admin::BaseController
+    before_action :set_announcement
+    before_action :set_new_announcement_organ, only: [:new, :create]
+
+    def index
+      @announcement_organs = @announcement.announcement_organs
+    end
+
+    private
+    def set_announcement
+      @announcement = Announcement.find params[:announcement_id]
+    end
+
+    def set_departments
+      @departments = Org::Department.default_where(default_params)
+    end
+
+    def set_new_announcement_organ
+      @announcement_organ = @announcement.announcement_organs.build(announcement_organ_params)
+    end
+
+    def announcement_organ_params
+      _p = params.fetch(:announcement_organ, {}).permit(
+        :organ_id
+      )
+      _p.with_defaults! default_form_params
+    end
+
+  end
+end
