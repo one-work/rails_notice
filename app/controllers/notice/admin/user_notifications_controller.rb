5e8b6b90c7e66a35b340c25a94659753de426791
qinmingyuan <mingyuan0715@foxmail.com>
2026-07-11 14:50:57 +0800

    user noticifaciton

---
+++ app/controllers/notice/admin/user_notifications_controller.rb
@@ -0,0 +1,43 @@
+module Notice
+  class Admin::UserNotificationsController < Admin::BaseController
+    before_action :set_notification, only: [:show, :push, :email, :edit, :update, :destroy]
+
+    def index
+      q_params = {
+        archived: false
+      }
+      q_params.merge! default_params
+      q_params.merge! params.permit(:archived, :receiver_id, :notifiable_type, :notifiable_id, 'user.name-like')
+
+      @notifications = UserNotification.default_where(q_params).order(id: :desc).page(params[:page])
+    end
+
+    def push
+      @notification.process_job
+    end
+
+    def email
+      @notification.send_email
+    end
+
+    private
+    def q_params
+      q = {}
+      q.merge! params.permit(:id, 'body-like', :user_id)
+    end
+
+    def set_notification
+      @notification = UserNotification.find(params[:id])
+    end
+
+    def notification_params
+      params.fetch(:notification, {}).permit(
+        :title,
+        :body,
+        :link,
+        :read_at
+      )
+    end
+
+  end
+end
